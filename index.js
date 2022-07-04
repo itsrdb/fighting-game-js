2af330cbc3d7dc4a6e1f4ff09be99b80fa9c075f
Rohit D Borkar <rohitborkar00@gmail.com>
2022-07-05 02:31:01 +0530

    enemy health done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw(){
@@ -147,7 +148,8 @@ function animate(){
     //detect collision
     if(rectangularCollision({rectange1: player, rectange2: enemy}) && player.isAttacking){
         player.isAttacking = false
-        console.log('player.hit')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if(rectangularCollision({rectange1: enemy, rectange2: player}) && enemy.isAttacking){
