00950ad7d6086f53bd5ddcf92cae9debc5630905
Rohit D Borkar <rohitborkar00@gmail.com>
2022-07-04 19:33:42 +0530

    added gravity

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
+
 class Sprite {
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw(){
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
     update(){
         this.draw()
-        this.position.y += 10
+        this.velocity.y += gravity
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        }
     }
 }
 
@@ -30,7 +38,7 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
