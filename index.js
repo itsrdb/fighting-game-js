ca5a1702922fa3a2569eaa545587c8d7aaf1a9c5
Rohit D Borkar <rohitborkar00@gmail.com>
2022-07-05 12:11:26 +0530

    timer on game over

--- index.js
+++ index.js
@@ -120,23 +120,29 @@ function rectangularCollision({rectange1, rectange2}){
     )
 }
 
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health == enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if(player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    } else if(player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
 let timer = 60
+let timerId
 function decreaseTimer(){
     if(timer>0){
-        setTimeout(decreaseTimer, 1000)
+        timerId = setTimeout(decreaseTimer, 1000)
         timer -= 1
         document.querySelector('#timer').innerHTML = timer
     }
 
     if(timer === 0){
-        document.querySelector('#displayText').style.display = 'flex'
-        if(player.health == enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Tie'
-        } else if(player.health > enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-        } else if(player.health < enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-        }
+        determineWinner({player, enemy})
     }
 }
 
@@ -178,6 +184,11 @@ function animate(){
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    //game over based on health
+    if(enemy.health <=0 || player.health <=0){
+        determineWinner({player, enemy, timerId})
     }
 }
 
